6dad14055f15b9c3fc2174cbe9f255d1c8efb902
daoatzy <dafasetiadi65@gmail.com>
2023-11-07 20:57:11 +0700

    mod 2 no 11

--- src/Bioskop23.java
+++ src/Bioskop23.java
@@ -18,8 +18,8 @@ public class Bioskop23 {
         System.out.println("Panjang Baris " + barisPenonton.length);
       }
       System.out.println("Penonton pada baris ke-3: ");
-      for (int i = 0; i < penonton[2].length; i++) {
-        System.out.println(penonton[2][i]);
-      }
+     for(String i : penonton[2]){
+        System.out.println(i);
+     }
     }
 }
